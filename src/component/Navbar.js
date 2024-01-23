7dd997f3540e508e5feec6115acdf635960ee34b
shivendra-999 <salukheshiv123@gmail.com>
2024-01-23 12:50:18 +0530

    Updated NavBar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,80 @@
 import React from "react";
+import "./NavBar.css";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+export default function Navbar () {
+  return (
+  <>
+      <div>
+        <nav
+          className="navbar navbar-expand-lg" >
+          <a className="navbar-brand" href="#">
+            <img className="logo" src="/images/logo.jpg" alt="Logo" />
+          </a>
+          <div className="container-fluid">
+            <ul className="navbar-nav ms-auto">
+              <li>
+                <a
+                  className="navBtn"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  className="navBtn"
+                  href="#"
+                >
+                  About us
+                </a>
+              </li>
+              <li>
+                <a
+                  className="navBtn"
+                  href="#"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  className="navBtn"
+                  href="#"
+                >
+                  Products
+                </a>
+              </li>
+              <li>
+                <a
+                  className="navBtn"
+                  href="#"
+                >
+                  Solutions
+                </a>
+              </li>
+              <li>
+                <a
+                  className="navBtn"
+                  href="#"
+                >
+                  Career
+                </a>
+              </li>
+              <li>
+                <a
+                  className="navBtn"
+                  href="#"
+                >
+                  Contact us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </>
+    )
 };
 
-export default Navbar;
+
