53eb78db2026ced3cb19e3a3a91a03bcc9889560
asharanjith <ashasvijayan@gmail.com>
2024-01-23 19:13:11 +0400

    Add text

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -4,7 +4,14 @@ import frameHome from "../../assets/frameHome.png";
 const Home = () => {
   return (
     <div id="homePage">
-      <p>Left side content</p>
+      <div>
+        <h1>Simplify Your Product Journey with Carenbit</h1>
+        <p>
+          Explore Carenbit's PLM Solutions for Enhanced Product Lifecycle
+          Management.
+        </p>
+        <button>Quick Inquiry</button>
+      </div>
       <img src={frameHome} alt="..." />
     </div>
   );
