f98622b2e68539a512baa52c0329cfc077f2587a
asharanjith <ashasvijayan@gmail.com>
2024-01-25 18:15:34 +0400

    bootstrap

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,18 +1,26 @@
 import React from "react";
+import "./Home.css";
 import frameHome from "../../assets/frameHome.png";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 
 const Home = () => {
   return (
     <div id="homePage">
+      <h1 class="mainHeading">
+        <small>
+          Simplify Your Product<br></br> Journey with Carenbit
+        </small>
+      </h1>
       <div>
-        <h1>Simplify Your Product Journey with Carenbit</h1>
         <p>
-          Explore Carenbit's PLM Solutions for Enhanced Product Lifecycle
-          Management.
+          Explore Carenbit's PLM Solutions for <br></br>Enhanced Product
+          Lifecycle Management.
         </p>
         <button>Quick Inquiry</button>
       </div>
-      <img src={frameHome} alt="..." />
+      <img src={frameHome} alt="..." class="plmImage" />
     </div>
   );
 };
