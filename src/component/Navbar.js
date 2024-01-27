4296936d9f608a7fc9b37693b7a6fc8c59eca67e
asharanjith <ashasvijayan@gmail.com>
2024-01-27 23:36:06 +0400

    Styling changes

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,80 +1,55 @@
 import React from "react";
 import "./NavBar.css";
 
-export default function Navbar () {
+export default function Navbar() {
   return (
-  <>
+    <>
       <div>
-        <nav
-          className="navbar navbar-expand-md" >
+        <nav className="navbar navbar-expand-md">
           <a className="navbar-brand" href="#">
             <img className="img-fluid" src="/images/logo.png" alt="Logo" />
           </a>
-          <div className="con">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  aria-current="page"
-                  href="#"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  About us
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  Services
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  Products
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  Solutions
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  Career
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#"
-                >
-                  Contact us
-                </a>
-              </li>
-            </ul>
-          </div>
+          {/* <div className="con"> */}
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Products
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Solutions
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Career
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact us
+              </a>
+            </li>
+          </ul>
+          {/* </div> */}
         </nav>
       </div>
     </>
-    )
-};
-
-
+  );
+}
