625364864da0f2944bb190a0818163a3debee01d
asharanjith <ashasvijayan@gmail.com>
2024-01-31 22:26:38 +0400

    add data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./component/Navbar";
 import Home from "./component/Home/Home";
+import AboutUs from "./component/AboutUs/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/component/aboutUs/AboutUs.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div id="aboutUsPage">
+      <div className="aboutTitle">
+        <h1>Welcome To Carenbit</h1>
+        <p>
+          Carenbit Technology Services, founded in 2022, is a pioneering startup
+          based in Pune's tech hub, Wakad. We specialize in providing expert
+          solutions in Teamcenter PLM, aiming to unlock the full potential of
+          businesses across diverse industries.
+        </p>
+      </div>
+      <div className="visionCard">
+        <img />
+        <div>
+          <h2>Vision</h2>
+          <p>
+            Our vision is to pioneer the Teamcenter PLM landscape, providing
+            clients with high-quality, innovative, and cost-effective solutions,
+            ultimately paving the path for their triumph.
+          </p>
+        </div>
+      </div>
+      <div className="missionCard">
+        <img />
+        <div>
+          <h2>Mission</h2>
+          <p>
+            At Carenbit, we propel our clients to unparalleled success in
+            product development by delivering tailor-made and comprehensive
+            Teamcenter PLM solutions, empowering businesses to thrive and excel
+            in the dynamic landscape of today’s evolving market.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
