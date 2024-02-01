6dcab64b10d3ee8327b85f3373c82fcc6a4baecd
asharanjith <ashasvijayan@gmail.com>
2024-02-01 18:15:27 +0400

    description change wrt screen size

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./component/Navbar";
 import Home from "./component/Home/Home";
 import AboutUs from "./component/aboutUs/AboutUs";
 
-function App() {
+const App = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
   return (
     <div className="App">
       <Navbar />
       <Home />
-      <AboutUs />
+      <AboutUs innerwidth={width} />
     </div>
   );
-}
+};
 
 export default App;
--- src/component/aboutUs/AboutUs.js
+++ src/component/aboutUs/AboutUs.js
@@ -3,21 +3,35 @@ import "./AboutUs.css";
 import mission from "../../assets/mission.png";
 import vision from "../../assets/vision.png";
 
-const AboutUs = () => {
+const AboutUs = (props) => {
+  const { innerwidth } = props;
+
   return (
     <div id="aboutUsPage">
       <div className="aboutTitle">
         <h1>Welcome To Carenbit</h1>
-        <p className="description">
-          Carenbit Technology Services, founded in 2022, is a pioneering startup
-          based in Pune's tech hub, Wakad. We specialize in providing expert
-          solutions in Teamcenter PLM, aiming to unlock the full potential of
-          businesses across diverse industries.
-        </p>
+        {innerwidth > 464 ? (
+          <p className="description my-4 ">
+            Carenbit Technology Services, founded in 2022, is a pioneering
+            startup based in Pune's tech hub, Wakad. We specialize in providing
+            expert solutions in Teamcenter PLM, aiming to unlock the full
+            potential of businesses across diverse industries.
+          </p>
+        ) : (
+          <p>
+            At Carenbit, we're driven by a singular passion: to empower
+            businesses with cutting-edge Product Lifecycle Management (PLM)
+            solutions that redefine excellence.
+          </p>
+        )}
       </div>
-      <div className="blockCard">
-        <img src={vision} alt="..." class="cardImage img-fluid" />
-        <div className="statements">
+      <div className="blockCard p-5">
+        <img
+          src={vision}
+          alt="..."
+          class="cardImage img-fluid bg-white rounded-3 p-3"
+        />
+        <div className="statements px-1">
           <h2>Vision</h2>
           <p>
             Our vision is to pioneer the Teamcenter PLM landscape, providing
@@ -26,8 +40,12 @@ const AboutUs = () => {
           </p>
         </div>
       </div>
-      <div className="blockCard reverse">
-        <img src={mission} alt="..." class="cardImage img-fluid" />
+      <div className="blockCard reverse p-5">
+        <img
+          src={mission}
+          alt="..."
+          class="cardImage img-fluid  bg-white rounded-3 p-3"
+        />
         <div className="statements">
           <h2>Mission</h2>
           <p>
